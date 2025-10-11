4bfb4c6ea94541cf101f90ce45c603d1edba8591
KUMBAN Marc Vivien Londonbell <kumbanmarcvivien@gmail.com>
2025-10-11 04:43:50 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 #define STYLE_ERROR_SENT "\033[1;91m"
 #define REINITIALISER "\033[0m"
 
-int main() {
+int main(void) {
     char input[MAX_INPUT];
 
     printf("\nBienvenue dans l'application ppmviewer!\n");
@@ -17,7 +17,7 @@ int main() {
 
     while (1) {
         printf("ppmviewer> ");
-        fflush(stdout); // s'assurer que l'invite s'affiche immédiatement
+        fflush(stdout);
 
         if (!fgets(input, sizeof(input), stdin)) {
             printf("\nProgramme interrompu.\n");
@@ -26,9 +26,9 @@ int main() {
 
         // Supprimer le retour chariot
         input[strcspn(input, "\n")] = '\0';
-        if (strlen(input) == 0) continue; // ignorer lignes vides
+        if (strlen(input) == 0) continue;
 
-        // Séparer la ligne en tokens
+        // Découper la commande
         char *commande = strtok(input, " ");
         char *arg1 = strtok(NULL, " ");
         char *arg2 = strtok(NULL, " ");
@@ -39,78 +39,103 @@ int main() {
 
         if (!commande) continue;
 
+        // ---- QUIT ----
         if (strcmp(commande, "quit") == 0) {
             printf("Fin du programme.\n");
             break;
         }
+
+        // ---- SIZE ----
         else if (strcmp(commande, "size") == 0) {
             if (!arg1) {
                 printf(STYLE_ERROR "Erreur : 'size' nécessite un nom de fichier.\n" REINITIALISER);
                 continue;
             }
 
-            int largeur = 0;
-            int hauteur = 0;
             InfoPix *infos = infoShow(arg1);
-            
-            largeur = infos->largeur;
-            hauteur = infos->hauteur;
-            printf(STYLE_RESULTAT "%d x %d\n" REINITIALISER, largeur, hauteur);
+            if (!infos) {
+                printf(STYLE_ERROR "Erreur : impossible de lire les informations du fichier %s.\n" REINITIALISER, arg1);
+                continue;
+            }
+
+            printf(STYLE_RESULTAT "%d x %d\n" REINITIALISER, infos->largeur, infos->hauteur);
         }
+
+        // ---- CUT ----
         else if (strcmp(commande, "cut") == 0) {
-            if (!arg2 || !arg3 || !arg4 || !arg5 || !arg6) {
-                printf(STYLE_ERROR "Erreur : 'cut' nécessite 6 arguments en entrée.\n" REINITIALISER);
+            if (!arg1 || !arg2 || !arg3 || !arg4 || !arg5 || !arg6) {
+                printf(STYLE_ERROR "Erreur : 'cut' nécessite 6 arguments.\n" REINITIALISER);
+                printf("Usage : cut <image.ppm> <l1> <l2> <c1> <c2> <sortie.ppm>\n");
                 continue;
             }
-            int ligne1 = atoi(arg2);
-            int ligne2 = atoi(arg3);
-            int colonne1 = atoi(arg4);
-            int colonne2 = atoi(arg5);
 
-            cut(arg1, ligne1, ligne2, colonne1, colonne2, arg6);
+            int l1 = atoi(arg2);
+            int l2 = atoi(arg3);
+            int c1 = atoi(arg4);
+            int c2 = atoi(arg5);
+
+            cut(arg1, l1, l2, c1, c2, arg6);
         }
+
+        // ---- DOM ----
         else if (strcmp(commande, "dom") == 0) {
-            if(!arg1 || !arg2 || !arg3) {
-                printf(STYLE_ERROR "La commande 'dom' nécessite 3 arguments en entrée.\n" REINITIALISER);
+            if (!arg1 || !arg2 || !arg3) {
+                printf(STYLE_ERROR "Erreur : 'dom' nécessite 3 arguments.\n" REINITIALISER);
+                printf("Usage : dom <R|V|B> <valeur> <image.ppm>\n");
                 continue;
             }
 
-            char dominante = (char)arg1;
+            char dominante = arg1[0]; // correction ici
             int valeur = atoi(arg2);
             foncerEclaircirDominante(dominante, valeur, arg3);
         }
+
+        // ---- NEG ----
         else if (strcmp(commande, "neg") == 0) {
-            if(!arg1 || !arg2) {
-                printf("La commande 'neg' nécessite deux arguments en entrée (le nom du fichier source et celui du fichier de sortie).\n");
+            if (!arg1 || !arg2) {
+                printf(STYLE_ERROR "Erreur : 'neg' nécessite 2 arguments.\n" REINITIALISER);
+                printf("Usage : neg <imageSource.ppm> <imageSortie.ppm>\n");
                 continue;
             }
 
             creerNegatif(arg1, arg2);
         }
+
+        // ---- GRIS ----
         else if (strcmp(commande, "gris") == 0) {
-            if(!arg1) {
-                printf("La commande 'gris' nécessite le nom du fichier source en entrée.\n");
+            if (!arg1) {
+                printf(STYLE_ERROR "Erreur : 'gris' nécessite un argument.\n" REINITIALISER);
+                printf("Usage : gris <imageSource.ppm>\n");
                 continue;
             }
 
             convertirEnGris(arg1);
         }
+
+        // ---- FILTRE MEDIAN ----
         else if (strcmp(commande, "fil") == 0) {
-            if(!arg1 || !arg2) {
-                printf("La commande 'fil' nécessite deux arguments en entrée (le nom du fichier source et celui du fichier de sortie).\n");
+            if (!arg1 || !arg2) {
+                printf(STYLE_ERROR "Erreur : 'fil' nécessite 2 arguments.\n" REINITIALISER);
+                printf("Usage : fil <imageSource.ppm> <imageSortie.ppm>\n");
+                continue;
             }
+
+            filtre_median(arg1, arg2);
         }
+
+        // ---- HELP ----
         else if (strcmp(commande, "help") == 0) {
-            printf("\nListe des commandes disponibles.\n\n");
-            printf("size <nomFichier> : Affiche la taille du l'image contenue dans le fichier.\n");
-            printf(
-                "cut <nomImage.ppm> <l1> <l2> <c1> <c2> <nomImageSortie.ppm> : Coupe les lignes entre l1 et l2 et colonnes entre c1 et c2.\n"
-            );
-            printf("dom <caractere> <valeur> <imageSource.ppm> : Fonce ou éclaircir la dominate d'un pixel de ImageSource.\n");
-            printf("neg <imageSource.ppm> <imageSortie.ppm> : crée le négatif du premier argument dans le deuxieme.\n");
-            printf("gris <imageSource.ppm> : convertie l'image source en gris.\n");
-            printf("fil <imageSource.ppm> <imageSortie.ppm> : applique le filtre median à image source et le résultat dans imageSortie.ppm.\n");
+            printf("\n--- Liste des commandes disponibles ---\n\n");
+            printf("size <fichier.ppm>                 : Affiche la taille de l'image.\n");
+            printf("cut <image> <l1> <l2> <c1> <c2> <sortie> : Coupe une partie de l'image.\n");
+            printf("dom <R|V|B> <valeur> <image>      : Fonce ou éclaircit une dominante.\n");
+            printf("neg <source> <sortie>             : Crée le négatif de l'image.\n");
+            printf("gris <source>                     : Convertit l'image en niveaux de gris.\n");
+            printf("fil <source> <sortie>             : Applique le filtre médian.\n");
+            printf("quit                              : Quitte l'application.\n\n");
         }
+
+        // ---- COMMANDE INCONNUE ----
         else {
             printf(STYLE_ERROR_SENT "Commande inconnue : " STYLE_ERROR "%s\n" REINITIALISER, commande);
             printf("Entrez 'help' pour obtenir de l'aide.\n");
